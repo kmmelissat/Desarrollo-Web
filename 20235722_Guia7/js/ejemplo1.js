6704097e2e0637684073a18a7e88618bbfc757ff
kmmelissat <20235722@esen.edu.sv>
2024-11-03 21:32:41 -0600

    bro

---
+++ 20235722_Guia7/js/ejemplo1.js
@@ -0,0 +1,29 @@
+// ACCEDIENDO A LA REFERENCIA DEL FORMULARIO QUE
+// TENDRA LOS NUEVOS ELEMENTOS
+const newForm = document.getElementById("idNewForm");
+
+// ACCEDIENDO A LA REFERENCIA DE BOTONES
+const buttonCrear = document.getElementById("idBtnCrear");
+const buttonAddElemento = document.getElementById("idBtnAddElement");
+
+// ACCEDIENDO AL VALOR DEL SELECT PARA DETERMINAR EL TIPO DE ELEMENTO A CREAR
+const cmbElemento = document.getElementById("idCmbElemento");
+
+// ACCEDIENDO A LOS CONTROLES DEL MODAL
+const tituloElemento = document.getElementById("idTituloElemento");
+const nombreElemento = document.getElementById("idNombreElemento");
+
+// CREANDO MODAL CON BOOTSTRAP
+const modal = new bootstrap.Modal(document.getElementById("idModal"), {});
+
+// AGREGANDO FUNCIONES
+const vericarTipoElemento = function () {
+    let elemento = cmbElemento.value;
+    //validando que se haya seleccionado un elemento
+    if (elemento != "") {
+    // Metodo perteneciente al modal de boostrap
+    modal.show();
+    } else {
+    alert("Debe seleccionar el elemento que se creara");
+    }
+    };
